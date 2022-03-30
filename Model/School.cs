0462651a87766a379cf5fb0a7eae0f5234b98a5a
Meryem Can <94848266+MeryemCanDOGAN@users.noreply.github.com>
2022-03-30 12:23:42 +0300

    Update School.cs

--- Model/School.cs
+++ Model/School.cs
@@ -8,12 +8,11 @@ namespace volunteer
         public int NumberofStudent { get; set; }
         public Teacher? Teacher { get; set; }
         public int? TeacherId { get; set; }
-         public Address? Address { get; set; }
+        public Address? Address { get; set; }
         public int? AddressId { get; set; }
         public virtual ICollection<ShoesSize>? ShoesSizes { get; set; }
         public virtual ICollection<CoatSize>? CoatSizes { get; set; }
-         public virtual ICollection<HatScarfGloveSize>? HatScarfGloveSizes { get; set; }
-       
+        public virtual ICollection<HatScarfGloveSize>? HatScarfGloveSizes { get; set; }
         public virtual ICollection<StationeryEquipmentList>? StationeryEquipmentLists { get; set; }
 
         
